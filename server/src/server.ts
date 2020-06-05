aa69e1e986d047d66606251cd5f7854257b10144
esterlilianlb <esterlilian2231@gmail.com>
2020-06-04 21:48:07 -0300

    primeiros conceitos e rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,35 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
+
+const users = [
+  'Diego',
+  'Cleiton',
+  'Robson',
+  'Daniel'
+];
 
 app.get('/users', (request, response) => {
   console.log('listagem de usuários')
 
-  response.json([
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Daniel'
-  ]);
+  response.json(users);
 });
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user)
+})
+
+app.post('/users', (request, response) => {
+  const user = {
+    name: 'Diego',
+    email: 'bla@bla.com'
+  };
+
+  return response.json(user);
+})
 
 app.listen(3333);
