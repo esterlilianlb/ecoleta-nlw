fc4240d6f917ce03822c1e603f0244c8a8bd4a23
esterlilianlb <esterlilian2231@gmail.com>
2020-06-08 16:41:18 -0300

    migrations criadas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,35 +1,10 @@
-import express, { request, response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-const users = [
-  'Diego',
-  'Cleiton',
-  'Robson',
-  'Daniel'
-];
+app.use(express.json());
+app.use(routes);
 
-app.get('/users', (request, response) => {
-  console.log('listagem de usuários')
-
-  response.json(users);
-});
-
-app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-
-  return response.json(user)
-})
-
-app.post('/users', (request, response) => {
-  const user = {
-    name: 'Diego',
-    email: 'bla@bla.com'
-  };
-
-  return response.json(user);
-})
 
 app.listen(3333);
